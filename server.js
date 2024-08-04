57871792ceadf59660bf1eb2669e3884b5e66028
Joshua Stanford <stanjosh@gmail.com>
2024-08-04 18:55:08 -0500

    feat: write/read json file

--- server.js
+++ server.js
@@ -2,10 +2,49 @@ const express = require('express');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
+const fs = require('fs');
+
+if (!fs.existsSync('./highscore.json')) {
+    fs.writeFileSync('./highscore.json', JSON.stringify([
+        {
+            "name": "josh",
+            "score": 100
+        },
+        {
+            "name": "phil",
+            "score": 200
+        }
+    ]));
+}
+
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/highscore", (req, res) => {
+    fs.createReadStream('./highscore.json').pipe(res);
+});
+
+app.post("/highscore", async (req, res) => {
+    var newScore = req.body;
+    highScoreFile.push(newScore);
+    fs.writeFile('./highscore.json', JSON.stringify(highScoreFile), (err) => {
+        if (err) {
+            console.log(err);
+        }
+    });
+    res.send("Score added");
+});
+
+
 
 app.get('*', (req, res) => {
     res.send('Hello, world!');
 });
+
+
+
 
 var server = app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
