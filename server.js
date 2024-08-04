e028ef2b7651620eda1ca129d3561ecf481a64f5
Joshua Stanford <stanjosh@gmail.com>
2024-08-04 18:24:53 -0500

    added close nicely function

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express = require('express');
-
+require('dotenv').config();
 const app = express();
-const port = process.argv[0] || process.env.PORT || 9944;
+const port = process.env.PORT || 3000;
 
 app.get('*', (req, res) => {
     res.send('Hello, world!');
 });
 
-app.listen(port, () => {
+var server = app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
+process.on('SIGTERM', shutDown);
+process.on('SIGINT', shutDown);
+
+function shutDown() {
+    server.close(() => {
+        process.exit(0);
+    });
+
+    setTimeout(() => {
+        console.error('Could not close connections in time, forcefully shutting down');
+        process.exit(1);
+    }, 10000);
+}
